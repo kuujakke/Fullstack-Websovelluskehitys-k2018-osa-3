4c877ea2878f44577fdb5c913247e17411009b9f
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2018-01-27 23:34:15 +0200

    Tehtävä 3.6

--- backend/index.js
+++ backend/index.js
@@ -55,10 +55,13 @@ app.delete('/api/persons/:id', (req, res) => {
 app.post('/api/persons', (req, res) => {
     let person = req.body
     person.id = nextId()
-    console.log(person)
-    persons = persons.concat(person)
-    res.json(persons)
-    console.log(persons)
+    let validation = validate(person)
+    if (validation.error === undefined) {
+        persons = persons.concat(person)
+        res.status(validation.status).json(persons)
+    } else {
+        res.status(validation.status).json(validation)
+    }
 })
 
 const nextId = () => {
@@ -73,6 +76,22 @@ const randomId = () => {
     let min = 1000000000
     let max = 9999999999
     return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+const validate = (person) => {
+    if (isEmpty(person.name)) {
+        return {error: "Name can't be empty!", status: 400}
+    } else if (isEmpty(person.number)) {
+        return {error: "Number can't be empty!", status: 400}
+    } else if (persons.find(p => p.name === person.name) !== undefined) {
+        return {error: "Person already exists", status: 403}
+    } else {
+        return {error: undefined, status: 201}
+    }
+}
+
+const isEmpty = (string) => {
+    return (string === undefined || string === '')
 }
 
 const PORT = 3001
