bf2fda7615049d2fc2051e197b7cb04157fe08cd
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2018-01-27 22:34:53 +0200

    Tehtävä 3.5

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let persons = [
     {
@@ -48,6 +51,29 @@ app.delete('/api/persons/:id', (req, res) => {
         res.status(404).end()
     }
 })
+
+app.post('/api/persons', (req, res) => {
+    let person = req.body
+    person.id = nextId()
+    console.log(person)
+    persons = persons.concat(person)
+    res.json(persons)
+    console.log(persons)
+})
+
+const nextId = () => {
+    let nextId = randomId()
+    while (persons.find(p => p.id === nextId) !== undefined) {
+        nextId = randomId()
+    }
+    return nextId
+}
+
+const randomId = () => {
+    let min = 1000000000
+    let max = 9999999999
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
 const PORT = 3001
 app.listen(PORT, () => {
